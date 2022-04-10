b893b45660ca3d2f7e243dbe75a48782228df784
Ismail <forwork.ismail@gmail.com>
2022-04-10 22:12:48 +0430

    Create useEffect to calculate total balance

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import History from './components/History';
 import MainHeader from './components/MainHeader';
 import NewTransaction from './components/NewTransaction';
 
 function App() {
   const [entries, setEntries] = useState(initialEntries);
+  const [total, setTotal] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [totalExpense, setTotalExpense] = useState(0);
 
   const addEntry = (description, value) => {
     const result = {
       id: entries.length + 1,
       description,
-      value
+      value: parseInt(value),
+      isExpense: false
     };
     setEntries([...entries, result]);
   };
+
+  useEffect(() => {
+    let totalIncomes = 0;
+    let totalExpenses = 0;
+    entries.map((entry) => {
+      if (entry.isExpense) {
+        return (totalExpenses += entry.value);
+      }
+      return (totalIncomes += entry.value);
+    });
+    let total = totalIncomes - totalExpenses;
+    setTotal(total);
+    setTotalIncome(totalIncomes);
+    setTotalExpense(totalExpenses);
+  }, [entries]);
+
   return (
     <div className="App container mx-auto px-4 py-10">
-      <MainHeader title="Budget" />
+      <MainHeader title="Budget" total={total} income={totalIncome} expense={totalExpense} />
       <div className="my-4 rounded-xl shadow-lg px-2 py-4 flex flex-col">
         <h3 className=" font-bold py-2 text-center">History</h3>
         {entries.map((entry) => {
@@ -34,25 +54,25 @@ const initialEntries = [
   {
     id: 1,
     description: 'Work income',
-    value: '$100',
+    value: 100,
     isExpense: false
   },
   {
     id: 2,
     description: 'Water bill',
-    value: '$20',
+    value: 20,
     isExpense: true
   },
   {
     id: 3,
     description: 'Rent',
-    value: '$300',
+    value: 300,
     isExpense: true
   },
   {
     id: 4,
     description: 'Power Bill',
-    value: '$50',
+    value: 50,
     isExpense: true
   }
 ];
--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function MainHeader({ title }) {
+function MainHeader({ title, total, income, expense }) {
   return (
     <div>
       <h1 className="text-3xl font-bold">{title}</h1>
       <div className="p-2">
         <h3 className="font-bold">Your Balance</h3>
-        <p className="text-green-400">2,550.3</p>
+        <p>{total}</p>
       </div>
       <div
         className="py-1 flex flex-row justify-evenly  rounded
@@ -14,11 +14,11 @@ function MainHeader({ title }) {
       >
         <div className="flex flex-col text-center">
           <span className=" font-semibold">Income:</span>
-          <span className="text-green-400 px-2 rounded">1,045.50</span>
+          <span className="text-green-400 px-2 rounded">{income}</span>
         </div>
         <div className="flex flex-col text-center">
           <span className=" font-semibold">Expense:</span>
-          <span className="text-red-400 px-2 rounded">623.3</span>
+          <span className="text-red-400 px-2 rounded">{expense}</span>
         </div>
       </div>
     </div>
